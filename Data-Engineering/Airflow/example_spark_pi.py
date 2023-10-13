3b048994f8cd9722d77892c71df4a1915e71a965
azubkov <anton.zubkov@hpe.com>
2023-10-13 16:47:33 +0300

    added simple example of DAG to write and read from volume (External DF)

--- Data-Engineering/Airflow/example_spark_pi.py
+++ Data-Engineering/Airflow/example_spark_pi.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.models.param import Param
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -22,14 +23,33 @@ dag = DAG(
     params={
         'airgap_registry_url': Param("", type=["null", "string"], pattern=r"^$|^\S+/$")
     },
-    render_template_as_native_obj=True
+    render_template_as_native_obj=True,
+    access_control={
+        'All': {
+            'can_read',
+            'can_edit',
+            'can_delete'
+        }
+    }
 )
 
 submit = SparkKubernetesOperator(
     task_id='submit',
     namespace="spark",
     application_file="example_spark_pi.yaml",
+    do_xcom_push=True,
     dag=dag,
     api_group="sparkoperator.hpe.com",
     enable_impersonation_from_ldap_user=True
 )
+
+sensor = SparkKubernetesSensor(
+    task_id='monitor',
+    namespace="spark",
+    application_name="{{ task_instance.xcom_pull(task_ids='submit')['metadata']['name'] }}",
+    dag=dag,
+    api_group="sparkoperator.hpe.com",
+    attach_log=True
+)
+
+submit >> sensor
---
+++ Data-Engineering/Airflow/example_write_read_datasources.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+with DAG(
+        'files_transfer',
+        default_args=default_args,
+        schedule_interval=None,
+        tags=['DF']
+) as dag:
+    file_path = "/mnt/datasources/test.txt"
+
+
+    @task
+    def write():
+        with open(file_path) as fp:
+            fp.write("Hello world")
+
+
+    @task
+    def read():
+        with open(file_path) as fp:
+            fp.read()
+
+
+    write() >> read()
