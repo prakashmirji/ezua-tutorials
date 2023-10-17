879663ff6d4f0ad30f5b403ef7f845e8b161de0f
azubkov <anton.zubkov@hpe.com>
2023-10-17 15:44:24 +0300

    added simple example of DAG to write and read from volume (External DF)

--- Data-Engineering/Airflow/example_write_read_datasources.py
+++ Data-Engineering/Airflow/example_write_read_datasources.py
@@ -1,6 +1,10 @@
 from airflow import DAG
+from airflow.models.param import Param
 from airflow.decorators import task
 from airflow.utils.dates import days_ago
+import logging
+
+logger = logging.getLogger(__name__)
 
 default_args = {
     'owner': 'airflow',
@@ -9,30 +13,36 @@ default_args = {
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
+    'max_active_runs': 1,
     'retries': 0
 }
 
 with DAG(
-        'files_transfer',
+        'write_read_file_external_df',
         default_args=default_args,
         schedule_interval=None,
-        tags=['DF']
+        tags=['ezaf', 'external DF'],
+        params={
+            'file_path': Param("/mnt/datasources/df/airflow_write_read_test.txt", type="string"),
+        }
 ) as dag:
-    file_path = "/mnt/datasources/df/test.txt"
+    file_path = {{dag_run.conf["file_path", "/mnt/datasources/df/airflow_write_read_test.txt"]}}
 
 
     @task
     def write():
         with open(file_path, 'w') as fp:
-            print("=====>>>  Started writing")
-            fp.write("Hello world")
-            print("=====>>>  Writing complete")
+            logger.info("Started writing file: ", file_path)
+            fp.write("This this test message to verify writing and reading file to External DF by Airflow DAG.")
+            logger.info("Writing complete")
 
 
     @task
     def read():
         with open(file_path) as fp:
-            print("=====>>> Content is: " + fp.read())
+            logger.info("Start reading file: ", file_path)
+            logger.info("Content of the file is: ", fp.read())
+            logger.info("Reading complete")
 
 
     write() >> read()
